f544d2fdccee583259e44b87b14c0445e5f0e590
Andy Scott <andy@andy-scott.com>
2014-06-04 23:38:32 +0100

    Added Commands
    Added the White and Colour Commands

---
+++ source/MiLight.Net.Api/ColourCommands.cs
@@ -0,0 +1,51 @@
+﻿namespace MiLight.Net.Api
+{
+    public static class ColourCommands
+    {
+        public static byte[] Off()
+        {
+            return new byte[] { 0x46, 0x00, 0x55 };
+        }
+
+        public static byte[] On()
+        {
+            return new byte[] { 0x45, 0x00, 0x55 };
+        }
+
+        public static byte[] Hue(decimal colourCode)
+        {
+            var hexColourCode = decimal.ToByte(16);
+            return new byte[] { 0x20, hexColourCode, 0x55 };
+        }
+
+        public static byte[] BrightUp()
+        {
+            return new byte[] { 0x23, 0x00, 0x55 };
+        }
+
+        public static byte[] BrightDown()
+        {
+            return new byte[] { 0x24, 0x00, 0x55 };
+        }
+
+        public static byte[] SpeedUp()
+        {
+            return new byte[] { 0x25, 0x00, 0x55 };
+        }
+
+        public static byte[] SpeedDown()
+        {
+            return new byte[] { 0x26, 0x00, 0x55 };
+        }
+
+        public static byte[] EffectUp()
+        {
+            return new byte[] { 0x27, 0x00, 0x55 };
+        }
+
+        public static byte[] EffectDown()
+        {
+            return new byte[] { 0x28, 0x00, 0x55 };
+        }
+    }
+}
---
+++ source/MiLight.Net.Api/WhiteCommands.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace MiLight.Net.Api
+{
+    public class WhiteCommands
+    {
+        public static byte[] AllOn()
+        {
+            return new byte[] { 0x35, 0x00, 0x55 };
+        }
+
+        public static byte[] AllOff()
+        {
+            return new byte[] { 0x39, 0x00, 0x55 };
+        }
+
+        public static byte[] BrightUp()
+        {
+            return new byte[] { 0x3c, 0x00, 0x55 };
+        }
+
+        public static byte[] BrightDown()
+        {
+            return new byte[] { 0x34, 0x00, 0x55 };
+        }
+
+        public static byte[] Warmer()
+        {
+            return new byte[] { 0x3E, 0x00, 0x55 };
+        }
+
+        public static byte[] Cooler()
+        {
+            return new byte[] { 0x3F, 0x00, 0x55 };
+        }
+
+        public enum Zone
+        {
+            One = 0,
+            Two = 1,
+            Three = 2,
+            Four = 3
+        }
+
+        public static byte[] On(Zone zone)
+        {
+            byte value;
+            switch (zone)
+            {
+                case Zone.One:
+                    value = 0x38;
+                    break;
+                case Zone.Two:
+                    value = 0x3D;
+                    break;
+                case Zone.Three:
+                    value = 0x37;
+                    break;
+                case Zone.Four:
+                    value = 0x32;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("zone");
+            }
+
+            return new byte[] { value, 0x00, 0x55 };
+        }
+
+        public static byte[] Off(Zone zone)
+        {
+            byte value;
+            switch (zone)
+            {
+                case Zone.One:
+                    value = 0x3B;
+                    break;
+                case Zone.Two:
+                    value = 0x33;
+                    break;
+                case Zone.Three:
+                    value = 0x3A;
+                    break;
+                case Zone.Four:
+                    value = 0x36;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("zone");
+            }
+
+            return new byte[] { value, 0x00, 0x55 };
+        }
+    }
+}
