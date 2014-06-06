1a010971ed792c4917e7012d6b321cae97cbf432
Andy Scott <andy@andy-scott.com>
2014-06-06 20:16:49 +0100

    Added a build file for myget

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild MiLight.Net.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+REM Package
+mkdir Build
+cmd /c %nuget% pack "MiLight.Net.Api\MiLight.Net.Api.csproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
